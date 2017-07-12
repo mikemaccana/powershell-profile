0c5949130c874df32052ee4bbfaa8e9bafb13626
Mike MacCana <mike@certsimple.com>
2017-07-12 10:59:46 +0100

    Add trash, whois, git-show-ignored, git-show-untracked, cut, make-link, ECC versions of openssl commands

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,7 +10,23 @@
 Add-PathVariable "${env:ProgramFiles}\OpenSSH"
 Add-PathVariable "${env:ProgramFiles}\rethinkdb"
 Add-PathVariable "${env:ProgramFiles}\7-Zip"
+Add-PathVariable "${env:ProgramFiles}\wtrace"
 Add-PathVariable "C:\OpenSSL-Win32\bin"
+Add-PathVariable "${env:ProgramFiles}\nodejs"
+# Add-PathVariable "${env:UserProfile}\AppData\Local\Atlassian\SourceTree\git_local\bin"
+
+
+#
+#
+# $env:NODE_PATH = "C:\Users\mike\AppData\Roaming\npm"
+
+# For 'Remove-ItemSafely' - ie, trashing files from the command line
+# Install-Module -Name Recycle
+Set-Alias trash Remove-ItemSafely
+
+function edit-powershell-profile {
+	subl $profile
+}
 
 # For working less (except in ISE)
 # Install-Package Pscx
@@ -19,8 +35,14 @@ Add-PathVariable "C:\OpenSSL-Win32\bin"
 # Install-Package PSReadLine
 Import-Module PSReadLine
 
+# https://gallery.technet.microsoft.com/WHOIS-PowerShell-Function-ed69fde6
+Unblock-File $home\scripts\whois.ps1
+. $home\scripts\whois.ps1
+
 # https://technet.microsoft.com/en-us/magazine/hh241048.aspx
 $MaximumHistoryCount = 10000
+
+
 
 # Tab completion for git
 # Install-Module posh-git
@@ -104,12 +126,12 @@ function explorer {
 }
 
 function gg {
-	& git grep -i @args
+	& git grep -n -i @args
 }
 
-# See https://jira.atlassian.com/browse/SRCTREEWIN-394 for some limits here
 function stree {
-	& "${env:ProgramFiles(x86)}\Atlassian\SourceTree\SourceTree.exe" -f $pwd
+	$SourceTreeCommand = (Get-ItemProperty HKCU:\Software\Classes\sourcetree\shell\open\command).'(default)'.split()[0].replace('"','')
+	& $SourceTreeCommand -f .
 }
 
 function open($file) {
@@ -144,6 +166,14 @@ function get-path {
 	($Env:Path).Split(";")
 }
 
+function git-show-ignored {
+	git ls-files . --ignored --exclude-standard --others
+}
+
+function git-show-untracked {
+	git ls-files . --exclude-standard --others
+}
+
 #######################################################
 # Unixlike commands
 #######################################################
@@ -152,12 +182,16 @@ function df {
 	get-volume
 }
 
+function cut($delimiter, $fieldNumber) {
+	$input | ForEach-Object { $_.split($delimiter)[$fieldNumber] }
+}
+
 function sed($file, $find, $replace){
 	(Get-Content $file).replace("$find", $replace) | Set-Content $file
 }
 
 function sed-recursive($filePattern, $find, $replace) {
-	$files = ls . "$filePattern" -rec
+	$files = ls . "$filePattern" -rec -Exclude
 	foreach ($file in $files) {
 		(Get-Content $file.PSPath) |
 		Foreach-Object { $_ -replace "$find", "$replace" } |
@@ -181,6 +215,14 @@ function which($name) {
 	Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
+function cut(){
+	foreach ($part in $input) {
+		$line = $part.ToString();
+		$MaxLength = [System.Math]::Min(200, $line.Length)
+		$line.subString(0, $MaxLength)
+	}
+}
+
 # Should really be name=value like Unix version of export but not a big deal
 function export($name, $value) {
 	set-item -force -path "env:$name" -value $value;
@@ -198,7 +240,12 @@ function touch($file) {
 	"" | Out-File $file -Encoding ASCII
 }
 
-# From https://github.com/keithbloom/powershell-profile/blob/master/Microsoft.PowerShell_profile.ps1
+# From https://stackoverflow.com/questions/894430/creating-hard-and-soft-links-using-powershell
+function make-link ($target, $link) {
+	New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
+# From https://github.com/keithbloom/powe	rshell-profile/blob/master/Microsoft.PowerShell_profile.ps1
 function sudo {
 	$file, [string]$arguments = $args;
 	$psi = new-object System.Diagnostics.ProcessStartInfo $file;
@@ -256,90 +303,129 @@ function pstree {
 	}
 }
 
-	function unzip ($file) {
-		$dirname = (Get-Item $file).Basename
-		echo("Extracting", $file, "to", $dirname)
-		New-Item -Force -ItemType directory -Path $dirname
-		expand-archive $file -OutputPath $dirname -ShowProgress
-	}
+function unzip ($file) {
+	$dirname = (Get-Item $file).Basename
+	echo("Extracting", $file, "to", $dirname)
+	New-Item -Force -ItemType directory -Path $dirname
+	expand-archive $file -OutputPath $dirname -ShowProgress
+}
 
 # From https://certsimple.com/blog/openssl-shortcuts
 function openssl-view-certificate ($file) {
+	echo "openssl x509 -text -noout -in $file"
 	openssl x509 -text -noout -in $file
 }
 
 function openssl-view-csr ($file) {
+	echo "openssl req -text -noout -verify -in $file"
 	openssl req -text -noout -verify -in $file
 }
 
-function openssl-view-key ($file) {
+function openssl-view-rsa-key ($file) {
+	echo openssl rsa -check -in $file
 	openssl rsa -check -in $file
 }
 
+function openssl-view-rsa-key ($file) {
+	echo "openssl rsa -check -in $file"
+	openssl rsa -check -in $file
+}
+
+function openssl-view-ecc-key ($file) {
+	echo "openssl ec -check -in $file"
+	openssl ec -check -in $file
+}
+
+
+
 function openssl-view-pkcs12 ($file) {
+	echo "openssl pkcs12 -info -in $file"
 	openssl pkcs12 -info -in $file
 }
 
 # Connecting to a server (Ctrl C exits)
 function openssl-client ($server) {
+	echo "openssl s_client -status -connect $server:443"
 	openssl s_client -status -connect $server:443
 }
 
 # Convert PEM private key, PEM certificate and PEM CA certificate (used by nginx, Apache, and other openssl apps)
 # to a PKCS12 file (typically for use with Windows or Tomcat)
 function openssl-convert-pem-to-p12 ($key, $cert, $cacert, $output) {
+	echo "openssl pkcs12 -export -inkey $key -in $cert -certfile $cacert -out $output"
 	openssl pkcs12 -export -inkey $key -in $cert -certfile $cacert -out $output
 }
 
 # Convert a PKCS12 file to PEM
 function openssl-convert-p12-to-pem ($p12file, $pem) {
+	echo "openssl pkcs12 -nodes -in $p12file -out $pemfile"
 	openssl pkcs12 -nodes -in $p12file -out $pemfile
 }
 
 # Convert a crt to a pem file
 function openssl-crt-to-pem($crtfile) {
-	v
+	echo "openssl x509 -in $crtfile -out $basename.pem -outform PEM"
 	openssl x509 -in $crtfile -out $basename.pem -outform PEM
 }
 
-# Check the modulus of a certificate (to see if it matches a key)
-function openssl-check-certificate-modulus {
+# Check the modulus of an RSA certificate (to see if it matches a key)
+function openssl-check-rsa-certificate-modulus {
+	echo "openssl x509 -noout -modulus -in "${1}" | shasum -a 256"
 	openssl x509 -noout -modulus -in "${1}" | shasum -a 256
 }
 
-# Check the modulus of a key (to see if it matches a certificate)
-function openssl-check-key-modulus {
+# Check the modulus of an ECDSA certificate (to see if it matches a key)
+function openssl-check-ecdsa-certificate-modulus {
+	echo "openssl x509 -noout -modulus -in "${1}" | shasum -a 256"
+	openssl x509 -noout -pubkey -in "${1}" | shasum -a 256
+}
+
+# Check the modulus of an RSA key (to see if it matches a certificate)
+function openssl-check-rsa-key-modulus {
+	echo "openssl rsa -noout -modulus -in "${1}" | shasum -a 256"
 	openssl rsa -noout -modulus -in "${1}" | shasum -a 256
+}
+
+# Check the modulus of an ECDSA key (to see if it matches a certificate)
+function openssl-check-rsa-key-modulus {
+	echo "openssl pkey -pubout -in "${1}" | shasum -a 256"
+	openssl pkey -pubout -in "${1}" | shasum -a 256
 }
 
 # Check the modulus of a certificate request
 function openssl-check-key-modulus {
+	echo openssl req -noout -modulus -in "${1}" | shasum -a 256
 	openssl req -noout -modulus -in "${1}" | shasum -a 256
 }
 
 # Encrypt a file (because zip crypto isn't secure)
 function openssl-encrypt () {
+	echo openssl aes-256-cbc -in "${1}" -out "${2}"
 	openssl aes-256-cbc -in "${1}" -out "${2}"
 }
 
 # Decrypt a file
 function openssl-decrypt () {
+	echo aes-256-cbc -d -in "${1}" -out "${2}"
 	openssl aes-256-cbc -d -in "${1}" -out "${2}"
 }
 
 # For setting up public key pinning
 function openssl-key-to-hpkp-pin() {
+	echo openssl rsa -in "${1}" -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64
 	openssl rsa -in "${1}" -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64
 }
 
 # For setting up public key pinning (directly from the site)
 function openssl-website-to-hpkp-pin() {
+	echo openssl s_client -connect "${1}":443 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
 	openssl s_client -connect "${1}":443 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
 }
 
 # Combines the key and the intermediate in a unified PEM file
 # (eg, for nginx)
 function openssl-key-and-intermediate-to-unified-pem() {
+	echo echo -e "$(cat "${1}")\n$(cat "${2}")" > "${1:0:-4}"_unified.pem
 	echo -e "$(cat "${1}")\n$(cat "${2}")" > "${1:0:-4}"_unified.pem
 }
 
