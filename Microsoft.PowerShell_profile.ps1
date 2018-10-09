ce704a64657cd628d3770b5b296b9ee2b6c21764
Mike MacCana <mike.maccana@gmail.com>
2018-10-09 12:57:02 +0100

    Prettify

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,92 +16,92 @@ $env:DOCUMENTS = [Environment]::GetFolderPath("mydocuments")
 New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
 
 # From https://serverfault.com/questions/95431/in-a-powershell-script-how-can-i-check-if-im-running-with-administrator-privil#97599
-function Test-Administrator  {  
-	$user = [Security.Principal.WindowsIdentity]::GetCurrent();
-	(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+function Test-Administrator {  
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
 }
 
 # Edit whole dir, so we can edit included files etc
 function edit-powershell-profile {
-	edit $profileDir
+  edit $profileDir
 }
 
 function update-powershell-profile {
-	& $profile
+  & $profile
 }
 
 # https://blogs.technet.microsoft.com/heyscriptingguy/2012/12/30/powertip-change-the-powershell-console-title
 function set-title([string]$newtitle) {
-	$host.ui.RawUI.WindowTitle = $newtitle + ' – ' + $host.ui.RawUI.WindowTitle
+  $host.ui.RawUI.WindowTitle = $newtitle + ' – ' + $host.ui.RawUI.WindowTitle
 }
 
 # From http://stackoverflow.com/questions/7330187/how-to-find-the-windows-version-from-the-powershell-command-line
 function get-windows-build {
-	[Environment]::OSVersion
+  [Environment]::OSVersion
 }
 
 function disable-windows-search {
-	Set-Service wsearch -StartupType disabled
-	stop-service wsearch
+  Set-Service wsearch -StartupType disabled
+  stop-service wsearch
 }
 
 # http://mohundro.com/blog/2009/03/31/quickly-extract-files-with-powershell/
 # and https://stackoverflow.com/questions/1359793/programmatically-extract-tar-gz-in-a-single-step-on-windows-with-7zip
 function expand-archive([string]$file, [string]$outputDir = '') {
-	if (-not (Test-Path $file)) {
-		$file = Resolve-Path $file
-	}
+  if (-not (Test-Path $file)) {
+    $file = Resolve-Path $file
+  }
 
-	$baseName = get-childitem $file | select-object -ExpandProperty "BaseName"
+  $baseName = get-childitem $file | select-object -ExpandProperty "BaseName"
 
-	if ($outputDir -eq '') {
-		$outputDir = $baseName
-	}
+  if ($outputDir -eq '') {
+    $outputDir = $baseName
+  }
 
-	# Check if there's a tar inside
-	# We use the .net method as this file (x.tar) doesn't exist!
-	$secondExtension = [System.IO.Path]::GetExtension($baseName)
-	$secondBaseName = [System.IO.Path]::GetFileNameWithoutExtension($baseName)
+  # Check if there's a tar inside
+  # We use the .net method as this file (x.tar) doesn't exist!
+  $secondExtension = [System.IO.Path]::GetExtension($baseName)
+  $secondBaseName = [System.IO.Path]::GetFileNameWithoutExtension($baseName)
 
-	if ( $secondExtension -eq '.tar' ) {
-		# This is a tarball
-		$outputDir = $secondBaseName
-		write-output "Output dir will be $outputDir"		
-		7z x $file -so | 7z x -aoa -si -ttar -o"$outputDir"
-		return
-	} 
-	# Just extract the file
-	7z x "-o$outputDir" $file	
+  if ( $secondExtension -eq '.tar' ) {
+    # This is a tarball
+    $outputDir = $secondBaseName
+    write-output "Output dir will be $outputDir"		
+    7z x $file -so | 7z x -aoa -si -ttar -o"$outputDir"
+    return
+  } 
+  # Just extract the file
+  7z x "-o$outputDir" $file	
 }
 
 set-alias unzip expand-archive
 
 function get-path {
-	($Env:Path).Split(";")
+  ($Env:Path).Split(";")
 }
 
 function Test-FileInSubPath([System.IO.DirectoryInfo]$Child, [System.IO.DirectoryInfo]$Parent) {
-	write-host $Child.FullName | select-object '*'
-	$Child.FullName.StartsWith($Parent.FullName)
+  write-host $Child.FullName | select-object '*'
+  $Child.FullName.StartsWith($Parent.FullName)
 }
 
 Set-Alias trash Remove-ItemSafely
 
 function open($file) {
-	invoke-item $file
+  invoke-item $file
 }
 
 function explorer {
-	explorer.exe .
+  explorer.exe .
 }
 
 function settings {
-	start-process ms-setttings:
+  start-process ms-setttings:
 }
 
 function stree {
-	$SourceTreeFolder =  get-childitem ("${env:LOCALAPPDATA}" + "\SourceTree\app*") | Select-Object -first 1
-	& $SourceTreeFolder/SourceTree.exe -f .
+  $SourceTreeFolder = get-childitem ("${env:LOCALAPPDATA}" + "\SourceTree\app*") | Select-Object -first 1
+  & $SourceTreeFolder/SourceTree.exe -f .
 }
 
 function get-serial-number {
@@ -112,8 +112,8 @@ function get-serial-number {
 #. "$profileDir\Get-NetworkStatistics.ps1"
 
 foreach ( $includeFile in ("openssl", "unix", "development", "node") ) {
-	Unblock-File $profileDir\$includeFile.ps1
-. "$profileDir\$includeFile.ps1"
+  Unblock-File $profileDir\$includeFile.ps1
+  . "$profileDir\$includeFile.ps1"
 }
 
 $documentsFolder = [Environment]::GetFolderPath("MyDocuments")
--- node.ps1
+++ node.ps1
@@ -23,23 +23,23 @@ Add-PathVariable $HOME\.windows-build-tools\python27
 # We use a locally installed mocha rather than a global one
 # Scope private do we don't call mocha recursively (just in case there is one in path)
 function Private:mocha() {
-	& node ..\..\node_modules\mocha\bin\mocha --ui tdd --bail --exit $args
+  & node ..\..\node_modules\mocha\bin\mocha --ui tdd --bail --exit $args
 }
 
 # Scope private do we don't call yarn recursively!
 function Private:yarn() {
-	$modifiedArgs = @()
-	foreach ( $arg in $args ) {
-		# yarn broke 'ls'
-		if ( $arg -cmatch '^ls' ) {
-			$arg = 'list'
-		}
-		$modifiedArgs += $arg
-		# we're using a monorepo, and only add packages to
-		# our workspace if we write them ourselves
-		if ( $arg -cmatch 'add' ) {
-			$modifiedArgs += '--ignore-workspace-root-check'
-		}
-	}
-	& yarn $modifiedArgs
+  $modifiedArgs = @()
+  foreach ( $arg in $args ) {
+    # yarn broke 'ls'
+    if ( $arg -cmatch '^ls' ) {
+      $arg = 'list'
+    }
+    $modifiedArgs += $arg
+    # we're using a monorepo, and only add packages to
+    # our workspace if we write them ourselves
+    if ( $arg -cmatch 'add' ) {
+      $modifiedArgs += '--ignore-workspace-root-check'
+    }
+  }
+  & yarn $modifiedArgs
 }
