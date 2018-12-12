d9e365550f46567a9f99315307c6a228f2542b55
Mike MacCana <mike.maccana@gmail.com>
2018-12-12 13:02:05 +0000

    Add snyk dependencies for path in node

--- node.ps1
+++ node.ps1
@@ -3,6 +3,8 @@ Add-PathVariable "${env:ProgramFiles}\nodejs"
 # Add relative node_modules\.bin to PATH - this allows us to easily use local bin files and less things installed globally
 Add-PathVariable '.\node_modules\.bin'
 
+# git also provides tools like 'patch' which is used by snyk
+Add-PathVariable "${env:ProgramFiles}\Git\usr\bin"
 
 # yarn bin folder
 # Disabled until https://github.com/yarnpkg/yarn/issues/6318 is fixed
